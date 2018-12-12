1a7d7525c0980f886d3d282c481715b8fd2fda1f
Alex <A01611451@itesm.mx>
2018-12-12 15:24:58 -0800

    Only Jump Once and a code Cleanup

---
+++ Test/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+	public Transform player;
+
+	/**
+	 * This camera will only follow the player on the X axis.
+	 * Can you figure out how to make it follow the player on the Y axis, too?
+	 */
+	private void Update()
+	{
+		Vector3 myPosition = transform.position;
+		Vector3 playerPosition = player.position;
+		transform.position = new Vector3(playerPosition.x, myPosition.y, myPosition.z);
+	}
+}
--- Test/Assets/Scripts/Player.cs
+++ Test/Assets/Scripts/Player.cs
@@ -4,41 +4,71 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    public float moveAmount;
-    public float forceAmount;
     public Rigidbody2D rb;
-    public float JumpAmount;
-    public Animator anmim; 
-    // Use this for initialization
+    public Animator anim;
+
+    [Header("Speed Settings")]
+    public float maxGroundSpeed;
+    public float maxAirSpeed;
+    private bool facingRight = true;
+
+    [Header("Ground Check Settings")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundCheckLayers;
+    private bool isStandingOnGround = false;
+
+    [Header("Jumping Settings")]
+    public float simpleJumpForce;
+    public float fallMultiplier = 2.5f;
+    public float jumpMultiplier = 2f;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        RigidbodyForce();
+        SimpleJump();
     }
 
-    void RigidbodyForce()
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(new Vector2(0, JumpAmount));
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(new Vector2(forceAmount, 0));
+        RunningMovement();
+    }
+    void LateUpdate()
+    {
+        CheckIfOnGround();
+    }
+    void RunningMovement()
+    {
+        float move = Input.GetAxis("Horizontal");
+        float maxSpeed = maxAirSpeed;
+        if (isStandingOnGround)
+            maxSpeed = maxGroundSpeed;
+        float speedMove = move * maxSpeed * Time.fixedDeltaTime;
+        rb.velocity = new Vector2(speedMove, rb.velocity.y);
 
-        }
-        if (Input.GetKey(KeyCode.A))
+    }
+    void CheckIfOnGround()
+    {
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.useTriggers = false;
+        Collider2D[] results = new Collider2D[2];
+        isStandingOnGround = false;
+        if (Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, filter, results) > 0)
         {
-            rb.AddForce(new Vector2(-forceAmount, 0));
-
+            foreach (Collider2D c in results)
+                if (c != null && c.attachedRigidbody != rb)
+                    isStandingOnGround = true;
         }
-
-        anmim.SetFloat("Speed", rb.velocity.x);
+    }
+    void SimpleJump()
+    {
+        if (isStandingOnGround && Input.GetButtonDown("Jump"))
+            rb.velocity = new Vector2(0, simpleJumpForce);
     }
 }
